4e8dba9382c2e7d26c67775b9bc7bcd412b74a7f
Awujoola <danielawujoola1@gmail.com>
2019-06-15 22:02:14 +0100

    vuex store

---
+++ client/src/store/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import auth from './modules/auth'
+
+Vue.use(Vuex)
+
+// create store
+export default new Vuex.Store({
+  modules: {
+    auth
+  }
+})
