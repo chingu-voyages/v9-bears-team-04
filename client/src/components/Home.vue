dfa0472d7646ac6cf4a25939796cd720063273af
Awujoola <danielawujoola1@gmail.com>
2019-06-16 03:08:27 +0100

    home page logout added

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,13 +1,19 @@
 <template>
     <div class="home">
-        <h2 v-if="isAuthenticated">Welcome {{ authUser.displayName }}</h2>
+      <div v-if="isAuthenticated">
+        <h2>Welcome {{ authUser.displayName }} </h2>
+        <button class="btn btn-google" @click="logout">Logout</button>
+      </div>
         <h2 v-else>Not Authenticated!</h2>
     </div>
 </template>
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 export default {
   name: 'Home',
-  computed: mapGetters(['authUser', 'isAuthenticated'])
+  computed: mapGetters(['authUser', 'isAuthenticated']),
+  methods: {
+    ...mapActions(['logout'])
+  }
 }
 </script>
