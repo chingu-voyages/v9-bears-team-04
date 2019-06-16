1fd3ddb1fa14a7048745297163d02fc4c8e77ade
Awujoola <danielawujoola1@gmail.com>
2019-06-16 03:00:14 +0100

    authentication details added

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,10 +1,13 @@
 <template>
     <div class="home">
-        <h2>User Authenticated!</h2>
+        <h2 v-if="isAuthenticated">Welcome {{ authUser.displayName }}</h2>
+        <h2 v-else>Not Authenticated!</h2>
     </div>
 </template>
 <script>
+import { mapGetters } from 'vuex'
 export default {
-  name: 'Home'
+  name: 'Home',
+  computed: mapGetters(['authUser', 'isAuthenticated'])
 }
 </script>
