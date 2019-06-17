d430181667e8182ec14349aabf1471591c1dd551
Igor Katselenbogen <orange-juice@Igors-MacBook-Pro.local>
2019-06-17 17:25:44 +0300

    Added page for books list and a way to reach it from Home

---
+++ client/src/components/BooksList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Books-List</h1>
+    <h2>ASDSD</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BooksList'
+}
+</script>
+
+<style scoped>
+
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
 import Home from '@/components/Home'
+import BooksList from '@/components/BooksList'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
@@ -16,6 +18,11 @@ export default new Router({
       path: '/home',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/books-list',
+      name: 'BooksList',
+      component: BooksList
     }
   ]
 })
