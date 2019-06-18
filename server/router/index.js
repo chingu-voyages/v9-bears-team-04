aec121ce1a8e4c6c646954e19c511a9d80774b66
Igor Katselenbogen <orange-juice@Igors-MacBook-Pro.local>
2019-06-18 22:00:39 +0300

    Added post routers - to add new book and to add new relation

--- server/router/index.js
+++ server/router/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const firebase = require('firebase-admin')
 const serviceAccount = require('../book-store-7dc95-firebase-adminsdk-modx9-89845da127')
+
 firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount),
   databaseURL: 'https://book-store-7dc95.firebaseio.com'
@@ -30,6 +31,36 @@ router.post('/api/v1/auth/google', (req, res) => {
   return res.status(200).send({
     success: true,
     message: 'User saved!'
+  })
+})
+
+router.post('/api/books-list', (req, res) => {
+  // book
+  const book = req.body
+
+  db.ref('tables').child('books/').push({
+    title: book.title,
+    author: book.author,
+    year: book.year
+  })
+  return res.status(200).send({
+    success: true,
+    message: 'Book saved!'
+  })
+})
+
+router.post('/api/user-book-rel', (req, res) => {
+  const rel = req.body
+  console.log(rel)
+
+  db.ref('tables').child('relations/').push({
+    userID: rel.userID,
+    bookID: rel.bookID,
+    status: rel.status
+  })
+  return res.status(200).send({
+    success: true,
+    message: 'Relation saved!'
   })
 })
 
